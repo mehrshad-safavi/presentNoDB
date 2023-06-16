b92eee81633232cac09d4bcea2f85bd615b00d4d
mehrshad <safavi.mehrshad.w@gmail.com>
2023-06-16 22:23:17 +0330

    created prizeRapper to work with prizes

---
+++ app/Services/PrizeWrapper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Prizes\BurnScore;
+use HaydenPierce\ClassFinder\ClassFinder;
+use Illuminate\Support\Collection;
+
+class PrizeWrapper
+{
+    /**
+     * return an object of class located in App\Services\Prizes that matches the id
+     * we could have made the class singleton and register the prize classes in serviceProvider!
+     */
+    function getClasses(): Collection
+    {
+        return collect(ClassFinder::getClassesInNamespace('App\Services\Prizes'));
+    }
+    function getInstance($prizeId): object
+    {
+        $classes = $this->getClasses();
+        $class = $classes->filter(function ($className) use ($prizeId) {
+            return $className::ID == (int)$prizeId;
+        });
+        return new ($class->first())();
+    }
+
+    function getAll(): array
+    {
+        $classes = $this->getClasses();
+        foreach ($classes as $class) {
+            $prizes[] = (new $class)->introduction();
+        }
+        return $prizes;
+    }
+}
